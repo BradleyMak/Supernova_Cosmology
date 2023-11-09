5d44d8fbd4d75d76b9289f47c1b7ac1cf6c5ebd0
Bradley <bradleymak2003@gmail.com>
2023-11-09 12:45:07 +0000

    Non-flat universe extension

--- functions.py
+++ functions.py
@@ -22,33 +22,45 @@ def read_data(filename):
     mag_err = np.array(alldata[:, 3], dtype = float)
     return name, redshift, eff_peak_mag, mag_err
 
+
+
 #function to calculate peak flux from effective peak magnitudes (and the respective errors)
 def get_flux(eff_peak_mag, mag_err, m_0):
     peak_flux = 10**(0.4*(m_0-eff_peak_mag))*10**-7*10**4 # in Wm^-2
     peak_flux_err = (10**(0.4*(m_0-(eff_peak_mag+mag_err)))*10**-7*10**4 - peak_flux) # in Wm^-2
     return peak_flux, peak_flux_err
 
+
+
 #function to calculate the comoving distance for a given redshift (approximation for z<0.1)
 def get_comoving_distance_low_z(redshift, H_0):
     comoving_distance = (c*redshift/H_0) #in m
     return comoving_distance
+
+
 
 #function to calculate the luminosity distance and its error from peak luminosity and flux.
 def get_luminosity_distance(redshift, comoving_distance):
     d_L = (1+redshift)*comoving_distance
     return d_L
 
+
+
 def get_norm_residuals(model_params, model_funct, x_data, y_data, y_err):
     norm_residuals = []
     for i in range(0,len(x_data)):
-        norm_residual = (y_data[i] - (model_funct(x_data[i], model_params)))/y_err[i]
+        norm_residual = (y_data[i] - (model_funct(x_data[i], *model_params)))/y_err[i]
         norm_residuals.append(norm_residual)
     norm_residuals = np.array(norm_residuals)
     return norm_residuals
 
+
+
 def chisq(model_params, model_funct, x_data, y_data, y_err):
         chisqval = (get_norm_residuals(model_params, model_funct, x_data, y_data, y_err))**2
         return chisqval
+
+
 
 def automated_curve_fitting(xval, yval, yerr, model_funct, initial, xlabel, ylabel):
     #order arrays in ascending order
@@ -84,7 +96,9 @@ def automated_curve_fitting(xval, yval, yerr, model_funct, initial, xlabel, ylab
     plt.legend()
     plt.show()
 
-    norm_residuals = get_norm_residuals(*popt, model_funct, xval, yval, yerr)
+    popt = popt.tolist()
+
+    norm_residuals = get_norm_residuals(popt, model_funct, xval, yval, yerr)
     chisq_min = np.sum(chisq(popt, model_funct, xval, yval, yerr))
     print('chi^2_min = {}'.format(chisq_min))
     chisq_reduced = chisq_min/deg_freedom
@@ -102,7 +116,7 @@ def automated_curve_fitting(xval, yval, yerr, model_funct, initial, xlabel, ylab
                  + f'DoF = {deg_freedom}' + '\n'
                  + '$χ^2_{red}$' + f'= {np.round(chisq_reduced, 2)}' + '\n' 
                  + f'P = {np.round(P, 3)}', 
-                 xycoords = 'axes fraction', xy = (0.7, 0.07), backgroundcolor = 'white')
+                 xycoords = 'axes fraction', xy = (0.65, 0.07), backgroundcolor = 'white')
     plt.ylabel(ylabel, weight = 'bold')
     plt.gca().set_xticks([])
 
@@ -117,7 +131,8 @@ Residuals""", weight = 'bold')
     plt.xlabel(xlabel, weight = 'bold')
 
     plt.figure(1).add_axes((0.8,-0.25,0.15,0.25))
-    plt.hist(norm_residuals, bins=np.arange(-3, 3, 0.5), alpha = 0.5, density = True, orientation = 'horizontal')
+    plt.hist(norm_residuals, bins=np.arange(-100, 100, 0.5), alpha = 0.5, density = True, orientation = 'horizontal')
+    plt.ylim(3, -3)
     plt.xticks([0.2,0.4], fontsize = 13)
     mu = 0; variance = 1
     sigma = math.sqrt(variance)
@@ -136,14 +151,29 @@ Density""", fontsize = 14, weight = 'bold')
     
     return popt, popt_errs
 
-def mag_model(redshift, L_peak, H_0, m_0, omega_lambda):
-    flux_predicted = L_peak/(4*np.pi*(1+redshift)**2*(integrate_array(comoving_distance_integrand, np.zeros(len(redshift)), redshift, args=(H_0, omega_lambda)))**2) #W/m^2/Ang
+
+
+def mag_model(redshift, L_peak, H_0, m_0, k, omega_lambda0, omega_M0):
+    flux_predicted = L_peak/(4*np.pi*(1+redshift)**2*(get_transverse_comoving_distance(redshift, H_0, k, omega_lambda0, omega_M0))**2) #W/m^2/Ang
     flux_predicted = flux_predicted * 10**7 * 10**-4 #erg/s/cm^2/Angstrom
     mag_predicted = m_0 - 2.5*np.log10(flux_predicted) 
     return mag_predicted
 
-def comoving_distance_integrand(z, H_0, omega_lambda):
-    return ((c)/(H_0*((1-(1+z)**3)*omega_lambda + (1+z)**3)**0.5))
+
+
+def comoving_distance_integrand(z, H_0, k, omega_lambda0, omega_M0):
+    if k == 0:
+        return ((c)/(H_0*((1-(1+z)**3)*omega_lambda0 + (1+z)**3)**0.5))
+    elif k == 1 or k == -1:
+        try:
+            R_0_sq = ((k*c**2)/(H_0**2*(omega_lambda0 + omega_M0 - 1)))
+            return ((c)/(H_0**2*(omega_M0*(1+z)**3+omega_lambda0)-k*c**2*(1+z)**2/R_0_sq**2)**0.5)
+        except:
+            return ((c)/(H_0*((1-(1+z)**3)*omega_lambda0 + (1+z)**3)**0.5))
+    else:
+        return ("k must equal 0, 1, or -1.")
+
+
 
 def integrate_array(function, lower_limit_array, upper_limit_array, args):
     output = []
@@ -152,12 +182,14 @@ def integrate_array(function, lower_limit_array, upper_limit_array, args):
         output.append(scipy.integrate.quad(function, lower_limit_array[i], upper_limit_array[i], args = args)[0])
     return np.array(output)
 
+
+
 def find_omega_lambda_and_error(x, actual, error, L_peak, H_0, m_0):
     no_of_values = 10000
     omega_lambdas = np.linspace(0, 1.1, no_of_values) #trial values for omega_lambda,0
     chi_squared = []
     for omega_lambda in omega_lambdas:
-        predicted = mag_model(x, L_peak, H_0, m_0, omega_lambda)
+        predicted = mag_model(x, L_peak, H_0, m_0, 0, omega_lambda, None)
         chi_squared.append(np.sum((actual-predicted)**2/error**2))
 
     min_index = np.argmin(chi_squared)
@@ -199,3 +231,24 @@ def find_omega_lambda_and_error(x, actual, error, L_peak, H_0, m_0):
     plt.show()
 
     return omega_lambda, omega_lambda_err
+
+
+
+def get_transverse_comoving_distance(redshift, H_0, k, omega_lambda0, omega_M0):
+    try:
+        r_c = integrate_array(comoving_distance_integrand, np.zeros(len(redshift).tolist()), redshift, args=(H_0, k, omega_lambda0, omega_M0))
+    except:
+        r_c = integrate_array(comoving_distance_integrand, [0], [redshift], args=(H_0, k, omega_lambda0, omega_M0))
+    if k == 0:
+        omega_M0 = 1 - omega_lambda0
+        return r_c
+    elif k == 1:
+        R_0 = ((k*c**2)/(H_0**2*(omega_lambda0 + omega_M0 - 1)))**0.5
+        return R_0*np.sin(r_c/R_0)
+    elif k == -1:
+        R_0 = ((k*c**2)/(H_0**2*(omega_lambda0 + omega_M0 - 1)))**0.5
+        return R_0*np.sinh(r_c/R_0)
+    else:
+        print("Invalid value of k encountered - k = -1, 0, or 1 only.")
+    
+
